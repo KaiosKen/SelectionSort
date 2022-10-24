ad3d74e3cb2c24692139e9c670ba2024c5ce1904
Robel Abraham <robel.abraham@student.allenisd.org>
2022-10-24 08:45:02 -0500

    Pushing all files to main

--- main.swift
+++ main.swift
@@ -1,48 +1,50 @@
-
-
-// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-let unsortedIntegers = [5, 1, 4, 2, 8]
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
-
-
-// Add your code below:
 import Foundation
+import Swift
 
-func SelectionSort(array: [Int])
+var randomWords : [String] = []
+
+// Read the contents of the specified file
+do {
+    //This do-catch program puts a txt file into an array and seperates it into multiple values
+    let data = try String(contentsOfFile:"random-10e4b.txt", encoding: String.Encoding.utf8)
+    randomWords = data.components(separatedBy: "\n")
+    randomWords.removeLast()
+} catch let err as NSError {
+    // do something with Error
+    print(err)
+
+}
+func SelectionSort(array: [String])
 {
-    var trueStatement = true
-    var p = 0
-    var swap = 0
+    
     var swapTotal = 0
     var unsortedArray = array
-    var sortedArray : [Int] = array
+    var sortedArray : [String] = array
     let arrayCount = array.count-1
     while !unsortedArray.isEmpty
     {
-       // print(unsortedArray)
-        print("Pass:  \(p), Swaps: \(swap)/\(swapTotal), Array: \(sortedArray)")
+  //      print(unsortedArray)
+  //     print("Pass:  \(p), Swaps: \(swap)/\(swapTotal), Array: \(sortedArray)")
         if let result = unsortedArray.min()
         {
             let unsortedResultIndex = unsortedArray.firstIndex(of: result)
             let resultIndex = sortedArray.firstIndex(of: result)
             var resultIndex2 : Int = 0
             var swapReady = false
+          
             for x in 0...arrayCount {
                 if result <= sortedArray[x] && !swapReady {
                     resultIndex2 = x
                     swapReady = true
                 }
-            }   
+            }
+            
             sortedArray.swapAt(resultIndex!, resultIndex2)
             swapTotal += 1
+           //  print(swapTotal)
             unsortedArray.remove(at: unsortedResultIndex!)
-            p += 1
-            if trueStatement {
-                swap += 1
-                trueStatement = false
-            }           
+           
+           
         }
         else
         {
@@ -50,4 +52,4 @@ func SelectionSort(array: [Int])
         }
     }     
 }
-SelectionSort(array: unsortedIntegers)
+SelectionSort(array: randomWords)
